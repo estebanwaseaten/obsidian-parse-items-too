bb6184c65f463d4a86de4431951808265d0a3d11
Daniel <daniel@wegkamp.de>
2026-01-02 15:18:11 +0000

    flow

--- src/itemary.ts
+++ src/itemary.ts
@@ -78,11 +78,11 @@ function extractItemsFromFrontmatter( file: TFile, frontmatter: any ): MyItem
         }
     }
 
-    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-    const sourcePath = view?.file?.path ?? "";
-    const link = this.app.fileManager.generateMarkdownLink( file, sourcePath );
+    //const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+    const sourcePath = file.path;
+    const link = this.app.fileManager.generateMarkdownLink( file, sourcePath, "", name );
 
-    console.log( "debug: " + sourcePath + "; " + link);
+    console.log( "debug: " + sourcePath + "; " + link );
 
 
     //parse detail better...
